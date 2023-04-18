0bc58f64c724b6194f5af457487621040f3e05b6
Ryohei Sasaki <rsasaki0109@gmail.com>
2023-04-18 19:33:31 +0900

    Bug fix

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
 # particle_filtering_model_predictive_control  
 Particle filtering model predictive control simulation.
 ~~~
-python particle_filtering_model_predictive_control.py
+python script/particle_filtering_model_predictive_control.py
 ~~~
-## to do  
- - fix state update  
- - fix horizon control  
- - delete index  
- - visualize predict trajectry  
  
 ## Result  
-![demo](./pf-mpc.gif)  
+![demo](./gif/pf-mpc.gif)  
+
 ## Reference  
 Dominik Stahl,JanHauth,"[PF-MPC: Particle filter-model predictive control](https://www.sciencedirect.com/science/article/pii/S0167691111001125)",2011  
 Simada,Nishida,"[PParticle Filter-Model Predictive Control of Quadcopters](https://ieeexplore.ieee.org/document/6911582)",2014  
--- script/particle_filtering_model_predictive_control.py
+++ script/particle_filtering_model_predictive_control.py
@@ -1,14 +1,6 @@
-"""
-
-Particle filtering model predictive control simulation.
-
-author: Ryohei Sasaki(@rsasaki0109)
-
-"""
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-
 
 k = 0.1  # look forward gain
 Lfc = 2.0  # look-ahead distance
@@ -60,11 +52,10 @@ def motion_model(x, u):
     return x
 
 def gauss_likelihood(x, sigma):
-    p = 1.0 / math.sqrt(2.0 * math.pi * sigma ** 2) * \
-        math.exp(-x ** 2 / (2 * sigma ** 2))
+    p = 1.0 / (2.0 * np.pi * np.linalg.det(sigma))**0.5 * \
+        np.exp(-0.5 * x.T @ np.linalg.inv(sigma) @ x)
 
     return p
-
 
 def resampling(px, pw):
     """
@@ -84,6 +75,7 @@ def resampling(px, pw):
                 ind += 1
             inds.append(ind)
 
+        inds = np.array(inds, dtype=int)  # Add this line
         px = px[:, inds]
         pw = np.zeros((1, NP)) + 1.0 / NP  # init weight
 
@@ -94,9 +86,9 @@ def target_trajectory(ix):
 
     return t
 
-def refrence_trajectory(state,x,ty):
+def refrence_trajectory(state, x, ty):
     Ts = 1
-    s = (ty - state.y)*(1 - math.exp(-(x - state.x)/Ts)) + state.y
+    s = (ty - state.y) * (1 - math.exp(-(x - state.x) / Ts)) + state.y
 
     return s
 
@@ -115,53 +107,43 @@ def pf_mpc(state, cx, cy, pind, px, pw, u):
         xp = np.array([px[:, ip]]).T
         w = pw[0, ip]
         for i in range(H):
-            i
             #  Predict with random input sampling
-            ud1 = u[0] + np.random.randn() * Rsim[0, 0] *4
-            ud2 = u[1] + np.random.randn()  *Rsim[1, 1] *4
+            ud1 = u[0] + np.random.randn() * Rsim[0, 0] * 4
+            ud2 = u[1] + np.random.randn() * Rsim[1, 1] * 4
             ud = np.array([[ud1, ud2]]).T
             xp = motion_model(xp, ud)
-            if(i==0):
+            if i == 0:
                 px[:, ip] = xp[:, 0].reshape(-1,)
-            if(i==1):    
-                pu[:, ip] = ud[:,0]
+            if i == 1:
+                pu[:, ip] = ud[:, 0]
 
         ty = target_trajectory(xp[0, 0])
-        s = refrence_trajectory(state,xp[0, 0],ty)
-        dist = abs(s- xp[1,0])
-        w =  gauss_likelihood(dist, math.sqrt(Q[0, 0])) 
+        s = refrence_trajectory(state, xp[0, 0], ty)
+        dist = abs(s - xp[1, 0])
+        w = gauss_likelihood(np.array([[dist]]), Q)
         pw[0, ip] = w
 
     pw = pw / pw.sum()  # normalize
 
     xEst = px.dot(pw.T)
-    state.x   = xEst[0]
-    state.y   = xEst[1]
+    state.x = xEst[0]
+    state.y = xEst[1]
     state.yaw = xEst[2]
-    state.v   = xEst[3]
+    state.v = xEst[3]
 
     px, pw = resampling(px, pw)
 
     i_wmax = np.argmax(pw)
 
     u = pu[:, i_wmax]
-    #x = np.array([[state.x, state.y, state.yaw, state.v]]).T
-    #x = motion_model(x, u)
-    #state.x   = x[0]
-    #state.y   = x[1]
-    #state.yaw = x[2]
-    #state.v   = x[3]
 
-    return state, ind, px, pw, u  
-
-
+    return state, ind, px, pw, u
 
 def calc_distance(state, point_x, point_y):
 
     dx = state.x - point_x
     dy = state.y - point_y
     return math.sqrt(dx ** 2 + dy ** 2)
-
 
 def calc_target_index(state, cx, cy):
 
@@ -198,7 +180,6 @@ def calc_target_index(state, cx, cy):
 
     return ind
 
-
 def plot_arrow(x, y, yaw, length=1.0, width=0.5, fc="r", ec="k"):
     """
     Plot arrow
@@ -212,7 +193,6 @@ def plot_arrow(x, y, yaw, length=1.0, width=0.5, fc="r", ec="k"):
                   fc=fc, ec=ec, head_width=width, head_length=width)
         plt.plot(x, y)
 
-
 def main():
     #  target course
     cx = np.arange(0, 50, 0.1)
@@ -223,9 +203,8 @@ def main():
     # initial state
     state = State(x=-0.0, y=-3.0, yaw=0.0, v=0.0)
     px = np.zeros((4, NP))  # Particle store
-    px[1,:]=state.y
+    px[1, :] = state.y
     pw = np.zeros((1, NP)) + 1.0 / NP  # Particle weight
-
 
     lastIndex = len(cx) - 1
     time = 0.0
@@ -240,7 +219,7 @@ def main():
 
     while T >= time and lastIndex - H > target_ind:
 
-        state, target_ind, px, pw, u = pf_mpc(state, cx, cy, target_ind, px, pw, u) 
+        state, target_ind, px, pw, u = pf_mpc(state, cx, cy, target_ind, px, pw, u)
 
         time = time + dt
 
@@ -280,7 +259,6 @@ def main():
         plt.ylabel("Speed[km/h]")
         plt.grid(True)
         plt.show()
-
 
 if __name__ == '__main__':
     print("Particle filtering model predictive control simulation start")
